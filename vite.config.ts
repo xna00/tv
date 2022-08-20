29fad591c48cf68e296ab2219dd04a6dcea938b4
xna <xna00@qq.com>
2022-08-20 22:30:23 +0800

    feat: add @unocss/preset-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import preact from "@preact/preset-vite";
 import Unocss from "unocss/vite";
-
+import presetUno from "@unocss/preset-uno";
+import presetIcons from "@unocss/preset-icons";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     preact(),
     splitVendorChunkPlugin(),
     Unocss({
-      /* options */
+      presets: [
+        presetUno(),
+        presetIcons({
+          // prefix: 'i',
+        }),
+      ],
     }),
   ],
   server: {
